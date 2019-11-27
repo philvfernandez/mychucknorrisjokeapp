0f75b6e9b5d3eb8191a1b70ddc6de28a4bc0a7bb
Phil Fernandez <philvfernandez@gmail.com>
2019-11-27 07:39:21 -0700

    Initial add of project.  closes $1

---
+++ src/main/java/guru/springframework/mychucknorrisjokeapp/MyChuckNorrisJokeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mychucknorrisjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChuckNorrisJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChuckNorrisJokeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/mychucknorrisjokeapp/MyChuckNorrisJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mychucknorrisjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChuckNorrisJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
