3df063ad0b096810ea35994d101dee5d75b7b7c0
Phil Fernandez <philvfernandez@gmail.com>
2019-12-19 16:41:06 -0700

    Created MVC Jokes Controller class and added a basic root mapping to jokes view which will be added later.

--- src/main/java/guru/springframework/mychucknorrisjokeapp/controller/ChuckNorriesJokeController.java
+++ src/main/java/guru/springframework/mychucknorrisjokeapp/controller/ChuckNorriesJokeController.java
@@ -1,4 +1,12 @@
 package guru.springframework.mychucknorrisjokeapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class ChuckNorriesJokeController {
+
+    @RequestMapping("/")
+    public ChuckNorriesJoke joke(@RequestParam(value = "joke") String name) {
+
+    }
 }
