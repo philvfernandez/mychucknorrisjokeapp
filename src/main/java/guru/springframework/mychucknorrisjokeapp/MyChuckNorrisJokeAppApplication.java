749a2c22271c16564c48aee8127311f076dac216
Phil Fernandez <philvfernandez@gmail.com>
2020-01-05 17:49:49 -0700

    Refactored to use a configuration based class with a bean that returns new instance of ChuckNorrisQuotes.

--- src/main/java/guru/springframework/mychucknorrisjokeapp/MyChuckNorrisJokeAppApplication.java
+++ src/main/java/guru/springframework/mychucknorrisjokeapp/MyChuckNorrisJokeAppApplication.java
@@ -3,8 +3,10 @@ package guru.springframework.mychucknorrisjokeapp;
 import guru.springframework.mychucknorrisjokeapp.services.JokeServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class MyChuckNorrisJokeAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/guru/springframework/mychucknorrisjokeapp/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/mychucknorrisjokeapp/config/ChuckConfiguration.java
@@ -4,11 +4,11 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 public class ChuckConfiguration {
 
     // Bean that Returns new instance of ChuckNorrisQuotes
-    @Bean
+    // @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
